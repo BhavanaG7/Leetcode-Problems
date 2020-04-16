cd50c120e3fb0cfa95f9a86e63da6343dd1462ed
Bhavana G <63728568+BhavanaG7@users.noreply.github.com>
2020-04-17 05:12:04 +0530

    Update Sum of Square Number.py

--- Sum of Square Number.py
+++ Sum of Square Number.py
@@ -1,4 +1,5 @@
 '''
+Leetcode Link:https://leetcode.com/problems/sum-of-square-numbers/
 Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
 
 Example 1:
