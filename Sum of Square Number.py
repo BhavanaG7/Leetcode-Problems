0d53ed12e0555fdc2eba5238c4490ee37fd1f4ef
Bhavana G <63728568+BhavanaG7@users.noreply.github.com>
2020-04-17 05:10:49 +0530

    Add files via upload

---
+++ Sum of Square Number.py
@@ -0,0 +1,34 @@
+'''
+Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+
+Example 1:
+
+Input: 5
+Output: True
+Explanation: 1 * 1 + 2 * 2 = 5
+ 
+
+Example 2:
+
+Input: 3
+Output: False
+'''
+
+class Solution:
+    def judgeSquareSum(self, c):
+        import math
+
+        cc=int(math.sqrt(c))
+        l=0
+        r=cc
+
+        while l<=r:
+            mid=l**2+r**2
+
+            if mid== c:
+                return True
+            elif mid<c:
+                l+=1
+            else:
+                r-=1
+        return False
