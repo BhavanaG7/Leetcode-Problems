831a3fa55f23b0483a8874a697d182330e7830cb
Bhavana G <63728568+BhavanaG7@users.noreply.github.com>
2020-04-18 16:55:21 +0530

    Update 1010. Pairs of Songs With Total Durations Divisible by 60.py

--- 1010. Pairs of Songs With Total Durations Divisible by 60.py
+++ 1010. Pairs of Songs With Total Durations Divisible by 60.py
@@ -1,6 +1,7 @@
 '''
 1010. Pairs of Songs With Total Durations Divisible by 60
 
+Link:https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
 In a list of songs, the i-th song has a duration of time[i] seconds. 
 
 Return the number of pairs of songs for which their total duration in seconds is divisible by 60.  Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.
